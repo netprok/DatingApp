45f967b9ed35851978eef6554bc08088c31d091c
KAYOOM\faramarziadm <netpro@email.com>
2019-01-01 21:08:20 +0100

    p1

---
+++ DatingApp.API/Data/AuthRepository.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext context;
+        public AuthRepository(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<User> Login(string username, string password)
+        {
+            var user = await context.Users.FirstOrDefaultAsync(x => x.UserName == username.ToLower());
+            if (user == null)
+                return null;
+
+            return VerifyPasswordHash(password, user);
+        }
+
+        private User VerifyPasswordHash(string password, User user)
+        {
+            using (var hmac = new HMACSHA512(user.PasswordSalt))
+            {
+                if (!user.PasswordHash.SequenceEqual(hmac.ComputeHash(Encoding.UTF8.GetBytes(password))))
+                    return null;
+            }
+
+            return user;
+        }
+
+        public async Task<User> Register(User user, string password)
+        {
+            CreatePasswordHash(password, out var passwordHash, out var passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await context.Users.AddAsync(user);
+            await context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public async Task<bool> UserExists(string username)
+        {
+            return await context.Users.AnyAsync(x => x.UserName == username);
+        }
+    }
+}
